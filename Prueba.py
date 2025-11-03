67a6d846b995813ffc8a4bca18f000ef22ad82cb
SantiagoPonce-git <aponcealbarracin@udesa.edu.ar>
2025-11-03 16:06:09 -0300

    modificacion de funcion actualizar posicion

--- Prueba.py
+++ Prueba.py
@@ -13,17 +13,22 @@ class Pajaro:
         self.x = 0
         self.vivo = True
 
-    def aleteo(self, delta_y, delta_x):
+    def decision_aleteo(self, delta_y, delta_x):
         w0,w1,w2,w3,w4,w5 = self.genes
         valor = w0 + w1*delta_y + w2*(delta_y**2)+w3*delta_x+w4*(delta_x**2) + w5 * self.vy
         return valor > 0 #Si es menor a cero no aletea, si es mayor a 0 aletea.
+    
+    def volar(self,fuerza_de_aleteo = -10):
+        self.vy = fuerza_de_aleteo
 
-    def actualizar_posicion(self, delta_y, delta_x, aleteo, gravedad = 0.5,fuerza_de_aleteo = -10):
-        if aleteo:
-            self.vy += fuerza_de_aleteo
-
+    def actualizar_posicion(self, delta_y, delta_x, aleteo, gravedad = 0.5):
         self.vy += gravedad
         self.y = self.vy
+
+        # # Chequear límites de pantalla
+        # if self.y < 0 or self.y > HEIGHT:
+        #     self.alive = False
+
 
         # <========== CASO DE COLISIONES ===============>
 
